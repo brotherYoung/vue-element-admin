f2992335450831f3611a7af3775fec0684a35326
jackson <xxx@qq.com>
2018-12-30 22:21:10 +0800

    ...

--- src/views/rtf/list.vue
+++ src/views/rtf/list.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="app-container">
 
+    <div class="filter-container">
+      <el-input :placeholder="('table.title')" v-model="listQuery.title" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter"/>
+      <el-select v-model="listQuery.type" :placeholder="('table.type')" clearable class="filter-item" style="width: 130px">
+        <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.display_name+'('+item.key+')'" :value="item.key"/>
+      </el-select>
+      <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit" @click="handleCreate">{{ ('table.add') }}</el-button>
+    </div>
+
     <el-table v-loading="listLoading" :data="list" border fit highlight-current-row style="width: 100%">
       <el-table-column align="center" label="ID" width="80">
         <template slot-scope="scope">
@@ -34,22 +42,22 @@
 
       <el-table-column min-width="300px" label="Title">
         <template slot-scope="scope">
-
           <router-link :to="'/rtf/edit/'+scope.row.id" class="link-type">
             <span>{{ scope.row.title }}</span>
           </router-link>
         </template>
       </el-table-column>
 
-      <el-table-column align="center" label="Actions" width="120">
+      <el-table-column label="Actions" align="center" width="230" class-name="small-padding fixed-width">
         <template slot-scope="scope">
           <router-link :to="'/rtf/edit/'+scope.row.id">
             <el-button type="primary" size="small" icon="el-icon-edit">Edit</el-button>
           </router-link>
+          <el-button type="danger" size="small" icon="el-icon-edit" @click="handleDelete">Delete</el-button>
         </template>
       </el-table-column>
     </el-table>
-    <!-- .sync 提供父子组件的双向绑定 -->
+    <!-- .sync 提供父子组件的双向数据绑定 -->
     <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
 
   </div>
@@ -58,6 +66,13 @@
 <script>
 import { fetchList } from '@/api/article'
 import Pagination from '@/components/Pagination' // Secondary package based on el-pagination
+
+const calendarTypeOptions = [
+  { key: 'CN', display_name: 'China' },
+  { key: 'US', display_name: 'USA' },
+  { key: 'JP', display_name: 'Japan' },
+  { key: 'EU', display_name: 'Eurozone' }
+]
 
 export default {
   name: 'ArticleList',
@@ -80,7 +95,8 @@ export default {
       listQuery: {
         page: 1,
         limit: 20
-      }
+      },
+      calendarTypeOptions
     }
   },
   created() {
@@ -101,6 +117,36 @@ export default {
     },
     handleCurrentChange(val) {
       this.listQuery.page = val
+      this.getList()
+    },
+    handleCreate() {
+      this.resetTemp()
+      this.dialogStatus = 'create'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    handleDelete() {
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        lockScroll: false,
+        type: 'warning'
+      }).then(() => {
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+    },
+    handleFilter() {
+      this.listQuery.page = 1
       this.getList()
     }
   }
